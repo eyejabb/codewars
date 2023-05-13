ead91d04a4987360ea6ed8d1f4c226dec51c08e8
Steven Fitzgerald <eyejabb@gmail.com>
2023-05-13 09:44:14 -0700

    endsWith method implemented

--- codewars/js/Completed Solutions/solution0047.js
+++ codewars/js/Completed Solutions/solution0047.js
@@ -1,5 +1,10 @@
-function doubleInteger(i) {
-  // i will be an integer. Double it and return it.
-  let twiceOver = i * 2
-  return twiceOver;
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+  return str.endsWith(ending)
 }
