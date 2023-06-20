2304c7177225cfd82985b34d1ee9abaaeb729004
Steven Fitzgerald <eyejabb@gmail.com>
2023-06-20 11:12:28 -0700

    codewars for the day

---
+++ codewars/js/Completed Solutions/solution0056.js
@@ -0,0 +1,12 @@
+function remove(string) {
+    let result = '';
+    
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] !== '!') {
+        result += string[i];
+      }
+    }
+    
+    result += '!';
+    return result;
+  }
