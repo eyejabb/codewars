3a9fe2ca92e7108b47fa1e5b0a37a665ae5fca82
Steven Fitzgerald <eyejabb@gmail.com>
2023-07-20 11:19:13 -0700

    more codewars

---
+++ codewars/js/Completed Solutions/solution0076.js
@@ -0,0 +1,8 @@
+function century(year) {
+    // Finish this :)
+    //take parameter of 'year'
+    //round 'year' up to the nearest integer
+    //split 'year' to get first two numbers from first two indexes
+    //join numbers to get result
+    return Math.ceil(year / 100);
+  }
