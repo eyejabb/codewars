03727b537a0322a726d6b4e910f5536ea63f6e2c
Steven Fitzgerald <eyejabb@gmail.com>
2023-08-18 10:31:00 -0700

    iterate over comments codewars

--- solution0066.js
+++ solution0066.js
@@ -1,3 +1,6 @@
+//Write a function that returns both the minimum and maximum number of the given list/array.
+//https://www.codewars.com/kata/559590633066759614000063/train/javascript
+
 function minMax(arr){
 return [Math.min(...arr) , Math.max(...arr)]
   }
