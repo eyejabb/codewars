afd8de9d0ec10592e80068658c58c12be8c4f594
Steven Fitzgerald <eyejabb@gmail.com>
2023-09-13 13:01:20 -0700

    encapsulation example

---
+++ codewars/js/Completed Solutions/solution0085.js
@@ -0,0 +1,30 @@
+class BankAccount {
+    constructor(initialBalance) {
+      let balance = initialBalance;
+  
+      this.deposit = (amount) => {
+        if (amount > 0) {
+          balance += amount;
+        }
+      };
+  
+      this.withdraw = (amount) => {
+        if (amount > 0 && amount <= balance) {
+          balance -= amount;
+        }
+      };
+  
+      this.getBalance = () => {
+        return balance;
+      };
+    }
+  }
+  
+  const account = new BankAccount(1000);
+  
+  account.deposit(500);
+  console.log(`Current balance: $${account.getBalance()}`);
+  
+  account.withdraw(200);
+  console.log(`Current balance: $${account.getBalance()}`);
+  
