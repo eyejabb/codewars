9fbc5723f395717d511b039f6693d0311439294b
Steven Fitzgerald <eyejabb@gmail.com>
2023-10-04 13:27:38 -0700

    codewars

--- codewars/js/Completed Solutions/solution0103.js
+++ codewars/js/Completed Solutions/solution0103.js
@@ -1,6 +1,11 @@
-function finalGrade (exam, projects) {
-    if(exam > 90 || projects > 10) return 100;
-    if(exam > 75 & projects >= 5) return 90;
-    if(exam > 50 & projects >= 2) return 75;
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (Number(exam) > 75 & projects >= 5) {
+    return 90;
+  } else if (Number(exam) > 50 & projects >= 2) {
+    return 75;
+  } else {
     return 0;
   }
+}
