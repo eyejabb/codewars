14f64ab8fbbe4e2700b20011dcef2cb954c4cdce
Steven Fitzgerald <eyejabb@gmail.com>
2023-12-04 16:01:48 -0800

    codewars

---
+++ solution0105.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+   let sum = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return sum;
+  }
